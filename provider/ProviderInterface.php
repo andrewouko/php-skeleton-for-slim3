d94dc6e78e6a6b6f32a65fcf577eb91fd58f21a3
Andrew Ouko <andrew.ouko.254@gmail.com>
2020-04-04 10:50:57 +0300

    Removed bugs noticed on installation to a second api

--- provider/ProviderInterface.php
+++ provider/ProviderInterface.php
@@ -4,7 +4,7 @@ use stdClass;
 use GuzzleHttp\Psr7\Request;
 interface ProviderInterface {
     //initialise provider using the environment and credentials
-    function __construct(string $environment, string $credentials_dir = '');
+    function __construct(string $environment, string $credentials_dir);
     //use named environments to initialise environments for providers
     function initialiseEnvironment(string $environment);
     //providers must return a PSr-7 request object given a certain input
