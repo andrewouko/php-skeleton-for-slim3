a4ca281aead36102d37b89f3d2e8cd9dc70b27bc
Andrew Ouko <andrew@opendoorafrica.com>
2020-05-26 23:51:03 +0300

    Response Handler fix

--- provider/Response.php
+++ provider/Response.php
@@ -88,7 +88,7 @@ abstract class Response {
      * @return ResponseInterface
      */
     protected function handleResponse(Container $container, GuzzleRequest $request = null, Google_Client $client = null):ResponseInterface{
-        $response_types = json_decode("VALID_RESPONSE_TYPES", true);
+        $response_types = json_decode(VALID_RESPONSE_TYPES, true);
         if($this->response_type == $response_types[0])
             $response = $this->getGuzzleResponse($container, $request);
         if($this->response_type == $response_types[1])
