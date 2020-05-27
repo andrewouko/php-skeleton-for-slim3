0ec09b082f5dfd67c4758982e37b20d04b0cada7
Andrew Ouko <andrew@opendoorafrica.com>
2020-05-27 23:29:58 +0300

    HTTP Validation Fixes

--- services/HTTP_Validation.php
+++ services/HTTP_Validation.php
@@ -3,6 +3,8 @@ namespace Services;
 
 use InvalidArgumentException;
 use DateTime;
+use RuntimeException;
+use stdClass;
 
 class HTTP_Validation {
     private $parameters;
@@ -14,9 +16,9 @@ class HTTP_Validation {
         if(!isset($this->parameters) || empty($this->parameters)) throw new InvalidArgumentException("The parameters array is not set");
         array_walk($this->parameters, function($metadata, $param) use ($input){
             if(!isset($input->$param)){
-                if($metadata->type != 'optional') throw new \InvalidArgumentException($param . " is required");
+                if(!$metadata->isOptional) throw new \InvalidArgumentException($param . " is required");
             }
-            if($metadata->type == 'numeric') $input->$param = (int) $input->$param;
+            // if($metadata->type == 'numeric') $input->$param = (int) $input->$param;
             if($metadata instanceof \stdClass){
                 if(isset($metadata->validate) && is_callable($metadata->validate)){
                     return $metadata->validate($input->$param);
@@ -28,7 +30,7 @@ class HTTP_Validation {
         });
         return true;
     }
-    private function validateParameter(string $type, string $name, &$val, int $length = null, array $valid_values = []){
+    private function validateParameter(string $type, string $name, bool $isOptional = false, &$val, int $length = null, array $valid_values = []){
         if(is_string($val) || is_numeric($val) || is_bool($val))
             $value_provided = gettype($val) . " of length " . strlen($val) . " provided. Value provided: " . $val;
 
@@ -54,9 +56,9 @@ class HTTP_Validation {
             case 'file':
                 if($val && isset($val->file) && is_file($val->file)) return true; else throw new InvalidArgumentException($name . " must be a valid file. File property of input is " . (isset($val->file) ? "set." : " not set.") . " File property of input is " . (is_file($val->file) ? " is a valid file." : " not a valid file."));
                 break;
-            case 'optional':
-                if((isset($val) && (gettype($val) != null && !is_null($length) && (strlen($val) <= $length))) || !isset($val)) return true; else throw new \InvalidArgumentException($name . " is optional and should have length of " . $length . ". " . $value_provided);
-                break;
+            // case 'optional':
+            //     if((isset($val) && (gettype($val) != null && !is_null($length) && (strlen($val) <= $length))) || !isset($val)) return true; else throw new \InvalidArgumentException($name . " is optional and should have length of " . $length . ". " . $value_provided);
+            //     break;
             default:
                 throw new \InvalidArgumentException($name . " is using an unsupported validation type " . $type);
         }
@@ -85,31 +87,57 @@ class HTTP_Validation {
                 return $this->parameters;
             case 'optional':
                 return array_filter($this->parameters, function($val){
-                    return $val->type == 'optional';
+                    return $val->isOptional;
                 });
             case 'required':
                 return array_filter($this->parameters, function($val){
-                    return $val->type != 'optional';
+                    return !$val->isOptional;
                 });
             default:
                 throw new \InvalidArgumentException("Invalid type of paramters to get. Valid types: ". json_encode($types));
 
         }
     }
-    function setParameterMetadata(){
-        //list of valid arguments that can be passed to this method
-        $valid_args = ['type', 'length', 'valid_values'];
+    function setParameterMetadata():stdClass{
         //get arguments passed in order
         $arguments = func_get_args();
-        $type = $length = null;
-        if(isset($arguments[0])) $type = $arguments[0];
-        if(isset($arguments[1])) $length = $arguments[1];
-        if(isset($arguments[2])) $valid_values = $arguments[2];
+        if(!count($arguments)) throw new InvalidArgumentException("Atleast one argument is required to setParameterMetadata.");
+
+        // init
         $metadata = array();
-        //validate arguments passed and assign their values to metadata array
-        foreach($valid_args as $arg){
+        $type = $length = $valid_values = null;
+        $indexes_from_length = 0;
+
+        // check whether the argument 1 is set to optional
+        if(isset($arguments[0]) && strtower($arguments[0]) == 'optional'){
+            $metadata['isOptional'] = true;
+            // check whether the 2nd argument is the length or type parameter
+            if(isset($arguments[1]) && is_string($arguments[1])){
+                // if string it is probably the type
+                $type = $arguments[1];
+                // cater for additional arguments
+                $indexes_from_length++;
+            } else {
+                // it is probably the length instead hence the type is automatically set
+                $type = 'alphanumeric';
+                // argument indexes remain the same
+            }
+        }
+        
+        if(!$type){
+            if(isset($arguments[0])) $type = $arguments[0];
+        }
+
+        if(isset($arguments[($indexes_from_length + 1)])) $length = $arguments[($indexes_from_length + 1)];
+        if(isset($arguments[($indexes_from_length + 2)])) $valid_values = $arguments[($indexes_from_length + 2)];
+        
+        //validate the metadata arguments after optional (if set) and assign their values to metadata array
+        foreach(['type', 'length', 'valid_values'] as $arg){
+            if(is_string($arg))
+                $arg = strtolower($arg);
             switch($arg){
                 case 'type':
+                    if(!isset($type)) throw new RuntimeException("Unable to determine the type of the paramter metadata. The type argument is required.");
                     if(!is_string($type)) throw new \InvalidArgumentException("The argument supplied to the 1st parameter (type) must be a string. Provided: " . gettype($type));
                     if($type) $metadata['type'] = $type;
                     break;
@@ -129,6 +157,6 @@ class HTTP_Validation {
                     throw new \InvalidArgumentException("Unsupported argument provided. Argument provided: " . $arg);
             }
         }
-        return count($metadata) > 0 ? (object) $metadata : null;
+        return (object) $metadata;
     }
 }
